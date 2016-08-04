fe151bb5ded82049614a5d160511665e57d83b5d
Mikhail Dubov <mdubov@mirantis.com>
2016-08-04 14:09:47 +0200

    Add comments to the server implementation

--- server/server.py
+++ server/server.py
@@ -7,11 +7,16 @@ from urlparse import urlparse, parse_qs
 
 class AnswerRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 
+    # TODO: Consider loading the NHS data only once when the server starts.
+    #       As of now, the data has to be loaded again at each request.
+
     def do_GET(self):
         request = urlparse(self.path)
         if request.path == '/answer':
+            # Parse the query
             query = parse_qs(request.query)["q"][0]
 
+            # Call the Java implementation of QuestionAnswerer
             output = subprocess.Popen(
                         [('java -Dfile.encoding=UTF-8 -classpath ' +
                           '"bin:lib/jsoup-1.9.2.jar:lib/json-simple-1.1.1.jar:lib/stanford-corenlp.jar" ' +
@@ -19,6 +24,7 @@ class AnswerRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
                           % query)],
                         stdout=subprocess.PIPE, shell=True).communicate()[0]
 
+            # Send the result back in JSON format
             self.send_response(200)
             self.send_header("Content-type", "text/json")
             self.send_header("Content-length", len(output))
