cf434b31666a449458c2459516fb136460503431
Mikhail Dubov <mdubov@mirantis.com>
2016-08-04 03:35:31 +0200

    Modify the scraper so that it scraps all the information from NHS
    Scrap not only common / child conditions, but all the pages from the index.

--- src/com/mikhail_dubov/nhs/NhsScraper.java
+++ src/com/mikhail_dubov/nhs/NhsScraper.java
@@ -16,7 +16,7 @@ import org.jsoup.select.Elements;
 
 /**
  * NHS website scraper that produces a JSON object containing various information
- * about most popular conditions listed there.
+ * about all the different conditions listed there.
  * 
  * @author Mikhail Dubov
  */
@@ -40,28 +40,30 @@ public class NhsScraper {
      * @throws IOException If there are connection issues.
      */
     public JSONObject scrape() throws IOException {
-        Document doc = Jsoup.connect(this.rootUrl).get();
         JSONObject res = new JSONObject();
-
-        Elements sections = doc.select("#LinkListZone div");
-        for (Element section : sections) {
-            String title = section.select("h2").first().text();
-            if (! title.toLowerCase().contains("condition")) {
-                // This allows to skip the NHS guides section,
-                // which it is particularly relevant.
-                continue;
-            }
-            JSONObject conditions = new JSONObject();
-            Elements conditionElements = section.select("ul li a");
-            for (Element conditionElement : conditionElements) {
-                String conditionName = conditionElement.text();
-                String conditionUrl = conditionElement.attr("href");
-                conditions.put(conditionName, this.scrapeCondition(conditionUrl));
-            }
-            res.put(title, conditions);
+        
+        // We use the index to get as many conditions as possible
+        for (char c = 'A'; c <= 'C'; c++) {
+        	// TODO: We could also use the link formatting on these pages
+        	//       to infer some structure (e.g. various types of cancer
+        	//       may be grouped togerther somehow).
+            scrapeIndexInto(String.valueOf(c), res);
         }
+        scrapeIndexInto("0-9", res);  // NOTE: the only non-alphabetical index item on NHS now
         
         return res;
+    }
+    
+    private void scrapeIndexInto(String index, JSONObject res) throws IOException {
+        Document doc = Jsoup.connect(this.rootUrl + "?Index=" + index).get();
+        Elements conditions = doc.select("#ctl00_PlaceHolderMain_BodyMap_ConditionsByAlphabet a");
+        for (Element condition : conditions) {
+            String conditionName = condition.text().replace("\u00a0","");
+            String conditionUrl = condition.attr("href");
+            if (conditionUrl.toLowerCase().contains("conditions/")) {
+                res.put(conditionName, this.scrapeCondition(conditionUrl));
+            }
+        }
     }
     
     /**
@@ -72,14 +74,26 @@ public class NhsScraper {
      * @throws IOException If there are connection issues.
      */
     private JSONObject scrapeCondition(String conditionPageUrl) throws IOException {
-        String fullUrl = "http://" + this.getBaseUrl() + conditionPageUrl;
+        if (! conditionPageUrl.startsWith("http")) {
+            conditionPageUrl = "http://" + this.getBaseUrl() + conditionPageUrl;
+        }
+        // Logging
+        System.out.println(conditionPageUrl);
         // We may sometimes have a java.net.SocketTimeoutException here,
-        // in this case a simple retrying helps.
-        Document doc = Jsoup.connect(fullUrl).followRedirects(true).ignoreHttpErrors(true).get();
+        // in this case we simply skip for now.
+        Document doc;
+        try {
+            doc = Jsoup.connect(conditionPageUrl).followRedirects(true).get();
+        } catch (Exception e) {
+            return new JSONObject();
+        }
 
         JSONObject res = new JSONObject();
-        res.put("URL", fullUrl);
-        res.put("Title", doc.select("h1").first().text().replace("\u00a0",""));
+        try {
+        	res.put("Title", doc.select("h1").first().text().replace("\u00a0",""));
+        } catch (NullPointerException e) {
+        	res.put("Title", "");
+        }
         // We don't just save the text but handle subsections, to keep our data structured.
         // NOTE: We reuse the scrapeSubSections() method both for the introductory page
         //       of each condition and for the pages describing symptoms / treatment / etc.
@@ -104,18 +118,34 @@ public class NhsScraper {
      * @throws IOException If there are connection issues.
      */
     private JSONObject scrapeSubSections(String pageUrl) throws IOException {
-        String fullUrl = "http://" + this.getBaseUrl() + pageUrl;
-        Document doc = Jsoup.connect(fullUrl).followRedirects(true).get();
+        if (! pageUrl.startsWith("http")) {
+            pageUrl = "http://" + this.getBaseUrl() + pageUrl;
+        }
+        // Logging
+        System.out.println(pageUrl);
+        Document doc;
+        try {
+            doc = Jsoup.connect(pageUrl).followRedirects(true).get();
+        } catch (Exception e) {
+            return new JSONObject();
+        }
 
-        Element content = doc.select(".main-content").first();
+        // NOTE: The formatting is not consistent in the NHS data
+        Element content = doc.select(".main-content, .article").first();
         Elements textElements = content.select("h3, p, ul");
 
         // Headers / texts are alternating in the HTML, so we should take care of that.
         JSONObject subSections = new JSONObject();
-        String currentSection = content.select("h2").first().text().replace("\u00a0","");
+        subSections.put("URL", pageUrl);
+        String currentSection;
+        try {
+        	currentSection = content.select("h2").first().text().replace("\u00a0","");
+        } catch (NullPointerException e) {
+        	return subSections;
+        }
         StringBuilder currentText = new StringBuilder();
         for (Element textElement : textElements) {
-            if (textElement.tagName().equals("h3")) {
+            if (textElement.tagName().equals("h3") || textElement.tagName().equals("h2")) {
                 subSections.put(currentSection, currentText.toString());
                 currentSection = textElement.text();
                 currentText = new StringBuilder();
@@ -123,6 +153,7 @@ public class NhsScraper {
                 currentText.append(textElement.text() + " ");
             }
         }
+        subSections.put(currentSection, currentText.toString());
         return subSections;
     }
     
@@ -140,7 +171,7 @@ public class NhsScraper {
      * Scraps the data from the NHS website and saves it into a JSON file.
      */
     public static void main(String[] args) throws IOException {
-        NhsScraper scraper = new NhsScraper("http://www.nhs.uk/Conditions/Pages/hub.aspx");
+        NhsScraper scraper = new NhsScraper("http://www.nhs.uk/Conditions/Pages/BodyMap.aspx");
         JSONObject data = scraper.scrape();
         BufferedWriter writer = null;
         try {
